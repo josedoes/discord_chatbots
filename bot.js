eaae06f1c64da76657f9e262760360d5ab952dde
joselara <jose@romantechnologies.co.uk>
2024-02-03 11:35:44 -0500

    updated message

--- bot.js
+++ bot.js
@@ -1,13 +1,8 @@
-// Import necessary libraries
 require('dotenv').config(); // Make sure to require dotenv at the top
 const { Client, Intents } = require('discord.js');
-const axios = require('axios');
 
-// Access your environment variables
 const token = process.env.DISCORD_BOT_TOKEN;
-const webhookURL = process.env.WEB_HOOK_URL;
 
-// Initialize Discord Bot
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
@@ -24,38 +19,12 @@ client.on('messageCreate', async message => {
         console.log(`Message received: ${message.content}`);
     }
 
-    // Check if the bot is mentioned and say hello
     if (message.mentions.users.has(client.user.id)) {
-        console.log('Bot was mentioned, saying hello...');
-        message.channel.send('Hello!').then(() => {
+        message.channel.send('Hey Guys! They are fixing LLM LAB right now, once the maintenace is complete Ill be able to talk!').then(() => {
             console.log('Greeting sent successfully.');
         }).catch(error => {
             console.error('An error occurred while sending the greeting:', error);
         });
-    }
-
-    // Simple command example: Responding to "!ping"
-    if (message.content === '!ping') {
-        console.log('!ping command received, sending response...');
-        message.channel.send('Pong!').then(() => {
-            console.log('Response sent successfully.');
-        }).catch(error => {
-            console.error('An error occurred while sending the message:', error);
-        });
-    }
-
-    // Example of using a webhook to send a message
-    if (message.content.startsWith('!webhook')) {
-        console.log('!webhook command received, attempting to send a webhook message...');
-
-        try {
-            const response = await axios.post(webhookURL, {
-                content: 'This is a message sent from a webhook triggered by a Discord command.',
-            });
-            console.log('Webhook message sent successfully:', response.data);
-        } catch (error) {
-            console.error('Failed to send webhook message:', error);
-        }
     }
 });
 
